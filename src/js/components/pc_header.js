3ace7997a1eb5c69a04b7e8cc1b39861a5346540
fergus <fergus.jingtai@gmail.com>
2017-06-08 05:25:18 +0800

    13-5 PC端登录功能模块开发

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -15,6 +15,7 @@ const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
+import {Router, Route, Link, browserHistory} from 'react-router'
 
 class PCHeader extends React.Component{
 	constructor() {
@@ -47,13 +48,28 @@ class PCHeader extends React.Component{
 		}
 		var formData = this.props.form.getFieldsValue()
 		console.log(formData)
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="+formData.r_userName+"&r_password="+formData.r_password+"&r_confirmPassword="+formData.r_confirmPassword,myFetchOptions)
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action 
+			+ "&username=" + formData.userName 
+			+ "&password=" + formData.password 
+			+ "&r_userName=" + formData.r_userName
+			+ "&r_password=" + formData.r_password
+			+ "&r_confirmPassword=" + formData.r_confirmPassword,myFetchOptions)
 			.then(response=>response.json())
 			.then(json => {
-				this.setState({userNickName: json.userNickName, userid: json.UserId})
+				this.setState({userNickName: json.NickUserName, userid: json.UserId})
 			})
+		if (this.state.action == "login") {
+			this.setState({hasLogined: true})
+		}
 		message.success("請求成功！")
 		this.setModalVisible(false)
+	}
+	callback(key) {
+		if (key == 1) {
+			this.setState({action: 'login'})
+		} else {
+			this.setState({action: 'register'})
+		}
 	}
   render() {
 		let { getFieldProps } = this.props.form;
@@ -112,7 +128,18 @@ class PCHeader extends React.Component{
 					<Col span={2}></Col>
 				</Row>
 				<Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText = "关闭">
-					<Tabs type="card">
+					<Tabs type="card" onChange={this.callback.bind(this)}>
+						<TabPane tab="登录" key="1">
+							<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+								<FormItem label="账户">
+									<Input placeholder="请输入您的账号" {...getFieldProps('userName')}/>
+								</FormItem>
+								<FormItem label="密码">
+									<Input type="password" placeholder="请输入您的密码" {...getFieldProps('password')}/>
+								</FormItem>
+								<Button type="primary" htmlType="submit">登录</Button>
+							</Form>
+						</TabPane>
 						<TabPane tab="注册" key="2">
 							<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
 								<FormItem label="账户">
@@ -124,7 +151,7 @@ class PCHeader extends React.Component{
 								<FormItem label="确认密码">
 									<Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
 								</FormItem>
-								<Button type="primary" htmlType="submit" >注册</Button>
+								<Button type="primary" htmlType="submit">注册</Button>
 							</Form>
 						</TabPane>
 					</Tabs>
