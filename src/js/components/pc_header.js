65f1dc0819979eb2a120f09373bbfbe875bfa5cf
fergus <fergus.jingtai@gmail.com>
2017-06-07 08:34:00 +0800

    13-2 注册功能模块设计（中）

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -28,6 +28,20 @@ class PCHeader extends React.Component{
 			userid: 0
 		}
 	}
+	setModalVisible(value) {
+		this.setState({modalVisible: value})
+	}
+	handleClick(e) {
+		if (e.key == 'register') {
+			this.setState({current: 'register'})
+			this.setModalVisible(true)
+		} else {
+			this.setState({current: e.key})
+		}
+	}
+	handleSubmit(e) {
+		//页面开始向 API 进行提交数据
+	}
   render() {
 		let { getFieldProps } = this.props.form;
 		const userShow = this.state.hasLogined
@@ -85,7 +99,22 @@ class PCHeader extends React.Component{
 					<Col span={2}></Col>
 				</Row>
 				<Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText = "关闭">
-					
+					<Tabs type="card">
+						<TabPane tab="注册" key="2">
+							<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+								<FormItem label="账户">
+									<Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
+								</FormItem>
+								<FormItem label="密码">
+									<Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
+								</FormItem>
+								<FormItem label="确认密码">
+									<Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
+								</FormItem>
+								<Button type="primary" htmlType="submit" >注册</Button>
+							</Form>
+						</TabPane>
+					</Tabs>
 				</Modal>
       </div>
     );
