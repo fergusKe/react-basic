fb9b4a6fbc11b393d0bb4372991f17dbd80835e1
fergus <fergus.jingtai@gmail.com>
2017-06-11 04:00:37 +0800

    15-2 PC 端详情页面开发（中）

--- src/js/components/pc_news_details.js
+++ src/js/components/pc_news_details.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Row, Col} from 'antd';
+import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
 export default class PCNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -22,6 +24,7 @@ export default class PCNewsDetails extends React.Component {
 	render() {
 		return (
 			<div>
+				<PCHeader></PCHeader>
 				<Row>
 					<Col span={2}></Col>
 					<Col span={14} className="container">
@@ -30,6 +33,7 @@ export default class PCNewsDetails extends React.Component {
 					<Col span={6}></Col>
 					<Col span={2}></Col>
 				</Row>
+				<PCFooter></PCFooter>
 			</div>
 		);
 	};
