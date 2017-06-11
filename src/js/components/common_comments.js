0b59f348758a813d142db1b552ddc7b92499c689
fergus <fergus.jingtai@gmail.com>
2017-06-12 06:23:07 +0800

    15-7 评论模块开发（下）

--- src/js/components/common_comments.js
+++ src/js/components/common_comments.js
@@ -9,7 +9,8 @@ import {
 	Input,
 	Button,
 	CheckBox,
-	Modal
+	Modal,
+	Card
 } from 'antd';
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
@@ -18,50 +19,54 @@ const MenuItemGroup = Menu.ItemGroup;
 import {Router, Route, Link, browserHistory} from 'react-router'
 class CommonComments extends React.Component {
 	constructor() {
-		super();
-		this.state = {
-			comments: ''
+        super();
+        this.state = {
+            comments: ''
+        };
+    };
+	componentDidMount() {
+		var myFetchOptions = {
+			method: 'GET'
 		};
-
-		componentDidMount() {
-			var myFetchOptions = {
-				method: 'GET'
-			};
-			fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
-				this.setState({comments: json});
-			})
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+			this.setState({comments: json});
+		});
+	};
+	handleSubmit(e) {
+		e.preventDefault();
+		var myFetchOptions = {
+			method: 'GET'
 		};
-
-
-		handleSubmit() {};
-		render() {
-			let {getFieldProps} = this.props.form;
-			const {commnets} = this.state;
-			const commnetList = commnets.length?
-			commnets.map((comment,index)=>(
-				<Card key={index} title={comment.UserName} extra={<a href="#">发布于 {commnet.datetime}</a>}>
-					<p>{comment.Commnets}</p>
+		var formdata = this.props.form.getFieldsValue();
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark, myFetchOptions).then(response => response.json()).then(json => {
+			this.componentDidMount();
+		})
+	};
+	render() {
+		let {getFieldProps} = this.props.form;
+		const {comments} = this.state;
+		const commnetList = comments.length
+			? comments.map((comment, index) => (
+				<Card key={index} title={comment.UserName} extra={<a href = "#"> 发布于 {comment.datetime} </a>}>
+					<p>{comment.Comments}</p>
 				</Card>
 			))
-			:
-			'没有加载到任何评论';
-			return (
-				<div class="comment">
-					<Row>
-						<Col span={24}>
+			: '没有加载到任何评论';
+		return (
+			<div class="comment">
+				<Row>
+					<Col span={24}>
 						{commnetList}
-							<Form onSubmit ={this.handleSubmit.bind(this)}>
-								<FormItem label="您的评论">
-									<Input type="textarea" placeholder="随便写" {...getFieldProps('remark',{initialValue: ''})}/>
-								</FormItem>
-								<Button type="primary" htmlType="submit">提交评论</Button>
-							</Form>
-						</Col>
-					</Row>
-				</div>
-			);
-		};
-	}
+						<Form onSubmit ={this.handleSubmit.bind(this)}>
+							<FormItem label="您的评论">
+								<Input type="textarea" placeholder="随便写" {...getFieldProps('remark',{initialValue: ''})}/>
+							</FormItem>
+							<Button type="primary" htmlType="submit">提交评论</Button>
+						</Form>
+					</Col>
+				</Row>
+			</div>
+		);
+	};
 }
-
 export default CommonComments = Form.create({})(CommonComments);
