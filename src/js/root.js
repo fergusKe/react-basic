83dd0ebb5293efb14b5670991adc0925f03dcdfc
fergus <fergus.jingtai@gmail.com>
2017-06-12 07:12:48 +0800

    16-1 PC 端用户个人中心开发

--- src/js/root.js
+++ src/js/root.js
@@ -6,6 +6,7 @@ import PCIndex from './components/pc_index';
 import PCNewsDetails from './components/pc_news_details';
 import MobileNewsDetails from './components/mobile_news_details';
 import MobileIndex from './components/mobile_index';
+import PCUserCenter from './components/pc_usercenter';
 import 'antd/dist/antd.css';
 import MediaQuery from 'react-responsive';
 
@@ -17,6 +18,7 @@ export default class Root extends React.Component {
 					<Router history={hashHistory}>
 						<Route path="/" component={PCIndex}></Route>
 						<Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+						<Route path="/usercenter" component={PCUserCenter}></Route>
 					</Router>
 				</MediaQuery>
 				<MediaQuery query='(max-device-width: 1224px)'>
